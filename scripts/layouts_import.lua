1867fa6c90103a44b5f66c95390d52697442e122
Oshroth <Oshroth@users.noreply.github.com>
2025-11-02 17:24:06 +1100

    Added autotracking and updated colour images (#2)

---
+++ scripts/layouts_import.lua
@@ -0,0 +1,10 @@
+-- Pipe Traversals
+Tracker:AddLayouts("layouts/pipetrav.json")
+-- Midway bells
+Tracker:AddLayouts("layouts/midwaybells.json")
+-- Secrets
+Tracker:AddLayouts("layouts/secret.json")
+-- Cancel Auto Scroll
+Tracker:AddLayouts("layouts/scroll.json")
+-- Settings
+Tracker:AddLayouts("layouts/settings_popup.json")
