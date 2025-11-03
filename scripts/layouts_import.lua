46b7f29e2f18a0a6ded1558ee900f8c2ab9465a7
Oshroth <Oshroth@users.noreply.github.com>
2025-11-03 22:27:37 +1100

    Add settings, dynamic layout and settings autotracking (#3)

--- scripts/layouts_import.lua
+++ scripts/layouts_import.lua
@@ -1,10 +1,18 @@
--- Pipe Traversals
-Tracker:AddLayouts("layouts/pipetrav.json")
--- Midway bells
-Tracker:AddLayouts("layouts/midwaybells.json")
 -- Secrets
 Tracker:AddLayouts("layouts/secret.json")
 -- Cancel Auto Scroll
 Tracker:AddLayouts("layouts/scroll.json")
 -- Settings
-Tracker:AddLayouts("layouts/settings_popup.json")
+Tracker:AddLayouts("layouts/settings_popup_coins.json")
+-- Default Main Grid
+Tracker:AddLayouts("layouts/main.json")
+-- Default Items
+Tracker:AddLayouts("layouts/items_coins.json")
+-- Default Midway bells
+Tracker:AddLayouts("layouts/midways.json")
+-- Tracker
+Tracker:AddLayouts("layouts/tracker.json")
+-- Broadcast
+Tracker:AddLayouts("layouts/broadcast.json")
+-- Map
+Tracker:AddLayouts("layouts/map.json")
